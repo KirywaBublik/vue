714f785fe26f4617755e9a5fe7231973badfa93f
Kirill Anisimov <kirillanisimov1337@yandex.ru>
2024-07-10 22:34:37 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1>Hello World!</h1>
+    <div>
+      <button>Increment</button>
+      <button>Decrement</button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
