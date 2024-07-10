f08adee64fe877bc3aba9dbd79a6f5b3b24b7779
Kirill Anisimov <kirillanisimov1337@yandex.ru>
2024-07-10 22:52:55 +0300

    counter

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
+
 <script setup lang="ts">
+
+import {onUpdated, ref} from "vue";
+
+  let counter = ref<number>(0)
+
+  onUpdated(() => {
+    if(counter.value < 0){
+      counter.value = 0
+    }
+  })
 
 </script>
 
 <template>
-    <h1>Hello World!</h1>
+  <div v-if="counter >= 10">
+    counter >= 10 | error
+  </div>
+  <div v-else>
+    <h1>{{ counter }}</h1>
     <div>
-      <button>Increment</button>
-      <button>Decrement</button>
+      <button @click="counter++">Increment</button>
+      <button @click="counter--">Decrement</button>
     </div>
+  </div>
+
 </template>
-
-<style scoped>
-
-</style>
